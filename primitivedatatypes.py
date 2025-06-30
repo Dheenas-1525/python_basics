eb0e6011ce8a33fe392cf4eba1d80905d9db91ac
Dheenas-1525 <dheenadhayalan0625@gmail.com>
2025-06-30 14:05:20 +0530

    changed some values

--- primitivedatatypes.py
+++ primitivedatatypes.py
@@ -59,3 +59,90 @@ print("this is Bool positive value :{}".format(bool(a)))
 
 a= 0 #this whole number thier is no floating values :
 print("this is Bool negative value :{}".format(bool(a)))
+
+
+
+'''next we recap for comparision opertor in pythonss....
+there are six types in comparision operator in python..such that
+== ->
+!= ->
+> ->
+< ->
+>= ->
+<= ->
+these types are basically used in true or false in python so that is called as comparision '''
+
+#using if else to see examples of comparison operators....
+
+a= 30
+
+b = 40
+
+if(a==b):
+    print("a is equal to b ")
+else:
+    print("a is not equal to b")
+
+if(a!=b):
+    print("yes a is not equal to b")
+else:
+    print("no a is equal to b")
+
+if(a>b):
+    print("yes a is greater than b")
+else:
+    print("no a is not greater than b")
+
+if(a<b):
+    print("yes a is less than b")
+else:
+    print("no a is less than b")
+
+if(a>=b):
+    print("yes a is grater than equal")
+else:
+    print("no its not greater than equal of a and b...")
+if(a<=b):
+    print("yes this less is  than a b")
+else:
+    print("no this is not a less than b ..")
+
+
+#next one is chaining comparision 
+
+''' this chaining comparision used like gates comparision examples 
+And Gates
+Or gates 
+Not gates '''
+
+'''first see examples of and gates 
+
+a b r 
+0 0 0
+0 1 0
+1 0 0
+1 1 1
+these is rules of and gates 
+'''
+
+#this is example of and gate using symbol of & 
+
+if(10<100 & 100>10): #1+1=1
+    print(True)
+else:
+    print(False)
+
+if(10<100 & 100<10): #1+0=0
+    print(True)
+else:
+    print(False)
+
+if(10>100 & 100>10):#0+1=0 
+    print(True)
+else:
+    print(False)
+
+if(10<100 & 100<10): #0+0=0
+    print(True)
+else:
+    print(False)
