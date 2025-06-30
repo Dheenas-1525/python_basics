b59632ce2089914861434d5da893dde3e2560702
Dheenas-1525 <dheenadhayalan0625@gmail.com>
2025-06-30 11:36:28 +0530

    datatypes

---
+++ primitivedatatypes.py
@@ -0,0 +1,43 @@
+#this section shown examples about primitive datatypes 
+"""
+primitive datatypes is about the arthmetic expression 
+like addition
+subtraction division and modules"""
+#first seen about simple arthmetic problem....
+a = 10
+
+b = 20
+
+print(a+b)#this is simple math
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+"""the above code is showed primitive datattypes in python"""
+
+
+'''order execution is main rules in computer programming 
+
+there is called as bodmas rules...
+
+b->bracket
+o->power
+d->division
+m->multiplication
+a->additon
+s->substraction '''
+print("im from bodmass rules")
+a=2
+b=3
+print((a+b**2+(a+b)**2)/2)
+
+
+#datatypes of python not for python only all programming lang using same syntax of datatypes
+'''
+integer
+float
+string
+boolean
+
+these are the datatypes in python'''
