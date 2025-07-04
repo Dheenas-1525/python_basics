f066b6c89292b1ed539dbe1eaaf0fee89298455b
Dheenas-1525 <dheenadhayalan0625@gmail.com>
2025-07-04 13:11:18 +0530

    file write

--- errorhandling.py
+++ errorhandling.py
@@ -19,3 +19,38 @@ finally:
     print("whatever the case is i will work")
 
 """ this above code os basic template of python error handling"""
+
+
+
+#class_error_handling_code
+
+
+
+def divide(a,b):
+    if b == 0:
+        raise ZeroDivisionError("cant divide by zero")
+        return a/b
+try:
+    c = divide(5, 0)
+    print("value of c is {}".format(c))
+except (NameError, IndexError, KeyError) as e:
+    print("NameError happened : {}".format(e))
+except ZeroDivisionError as e:
+    print("ZeroDivisionError: {}".format(e))
+else:
+    print("all good working code")
+
+finally:
+    print("subam......")
+
+l = [1,2,3,4,5,6,7,8,9]
+l = iter(l)
+
+print(next(l))
+print(next(l))
+print(next(l))
+print(next(l))
+print(next(l))
+print(next(l))
+print(next(l))
+print(next(l))
