a4cfbe4c3f035c6205ca140150c77d973a9d5bf9
Dheenas-1525 <dheenadhayalan0625@gmail.com>
2025-07-20 09:52:22 +0530

    collections

--- fun.py
+++ fun.py
@@ -2,3 +2,20 @@ def add(x, y):
     return x+y
 
 print(add(10, 20))
+
+
+
+l = ('dheena', 'arun', 'senthil')
+print(l)
+
+t = ['dheena', 'arun', 'senthil']
+print(t)
+
+s = {1,2,3,4,5}
+
+dic = {'name': 'dheena',
+'age':26,
+}
+
+print(s)
+print(dic)
